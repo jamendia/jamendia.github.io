c92de3230693322b8746a04938fcc4dcb75cd097
Jon Ander Mendia <4482857+jamendia@users.noreply.github.com>
2018-07-09 23:25:06 +0200

    fix typo

--- js/abstracts.js
+++ js/abstracts.js
@@ -1,8 +1,8 @@
 function hideshow(which){
-  if (!document.getElementById)
-	return
-  if (which.style.display=="block")
-	  which.style.display="none"
-  else
-	  which.style.display="block"
+	if (!document.getElementById)
+		return
+	if (which.style.display=="block")
+		which.style.display="none"
+	else
+		which.style.display="block"
 }
